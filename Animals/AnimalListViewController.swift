1316b8c11160bd9a27c633f393052ec43d49164a
Dougly <dgalante11@gmail.com>
2016-10-06 11:00:16 -0400

    lab complete

--- Animals/AnimalListViewController.swift
+++ Animals/AnimalListViewController.swift
@@ -9,4 +9,35 @@
 import UIKit
 
 class AnimalListViewController: UITableViewController {
+    
+    @IBOutlet var animalListTableView: UITableView!
+    
+    let animals: [String] = ["Dog", "Cat", "Mouse", "Hamster", "Bunny", "Panda", "Lion", "Pig", "Frog", "Octopus"]
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return animals.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "animalCell", for: indexPath)
+        cell.textLabel?.text = animals[indexPath.row]
+        return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "animalPush" {
+            let dest = segue.destination as! AnimalViewController
+            let indexPath = self.tableView.indexPathForSelectedRow
+            
+            //dest.animalEmoji = getEmojiForAnimal(animal: animals[(indexPath?.row)!])
+            dest.animalName = animals[(indexPath?.row)!]
+            //dest.navHeading = animals[(indexPath?.row)!]
+            
+            
+        }
+    }
 }
--- Animals/AnimalViewController.swift
+++ Animals/AnimalViewController.swift
@@ -9,4 +9,51 @@
 import UIKit
 
 class AnimalViewController: UIViewController {
+    
+    var animalName: String?
+    
+    @IBOutlet weak var viewObject: UIView!
+    @IBOutlet weak var animalNameLabel: UILabel!
+    @IBOutlet weak var animalEmojiLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.black
+        
+        if let animalName = animalName {
+            
+            animalNameLabel.text = animalName
+            animalEmojiLabel.text = getEmojiForAnimal(animal: animalName)
+            navigationItem.title = animalName
+        }
+        
+    }
+    
+    private func getEmojiForAnimal (animal: String) -> String {
+        switch animal.lowercased() {
+        case "dog":
+            return "🐶"
+        case "cat":
+            return "🐱"
+        case "mouse":
+            return "🐭"
+        case "hamster":
+            return "🐹"
+        case "bunny":
+            return "🐰"
+        case "panda":
+            return "🐼"
+        case "lion":
+            return "🦁"
+        case "pig":
+            return "🐷"
+        case "frog":
+            return "🐸"
+        case "octopus":
+            return "🐙"
+        default:
+            return "👻"
+        }
+    }
+
 }
